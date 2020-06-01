d6d3cf1d06f4a9a547178545ab1eac5c1200652a
camilomontoyau <camilomontoya2@gmail.com>
2020-05-31 21:21:43 -0500

    agregamos archivos css localmente

--- js/duenos.js
+++ js/duenos.js
@@ -96,3 +96,16 @@ listarDuenos();
 
 form.onsubmit = enviarDatos;
 btnGuardar.onclick = enviarDatos;
+
+async function registerSW() {
+  if('serviceWorker' in navigator) {
+    try {
+      const sw = await navigator.serviceWorker.register('service-worker.js');
+      console.log('sw registrado correctamente', { sw });
+    } catch (error) {
+      console.log('registro de service worker ha fallado', {error})
+    }
+  }
+}
+
+registerSW();
--- js/veterinarias.js
+++ js/veterinarias.js
@@ -96,3 +96,16 @@ listarVeterinarias();
 
 form.onsubmit = enviarDatos;
 btnGuardar.onclick = enviarDatos;
+
+async function registerSW() {
+  if('serviceWorker' in navigator) {
+    try {
+      const sw = await navigator.serviceWorker.register('service-worker.js');
+      console.log('sw registrado correctamente', { sw });
+    } catch (error) {
+      console.log('registro de service worker ha fallado', {error})
+    }
+  }
+}
+
+registerSW();
